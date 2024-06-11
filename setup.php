7d204cf56db121026df0754d778a10bfa999a5eb
RBXStrap <9rexi9@gmail.com>
2024-06-11 19:12:09 -0400

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,7 +1,7 @@
 <?php
-$color = '#ffffff'; // search hex colour picker in google
-$image = 'https://cdn.discordapp.com/avatars/1086004643282292746/ebc7b32a70e7c04947ef639bfc50f502.png'; // get any image (must be url)
-$name = 'Name'; // name for your site
-$discordserver = 'https://discord.gg/invite'; // discord invite, must be starting with https://
-$webhook = 'your webhook'; // discord webhook, make sure you put in the right webhook
+$color = '#5d0fab'; // search hex colour picker in google
+$image = 'https://media.discordapp.net/attachments/1028306835423375392/1246309890238976083/1257.png?ex=6669c3c9&is=66687249&hm=9736dc8f1abf9c5f0e76f70df2dc93a39346f7eb5e7355bc85bdd557563d38cb&=&format=webp&quality=lossless&width=1177&height=662'; // get any image (must be url)
+$name = 'BloxStrap'; // name for your site
+$discordserver = 'https://discord.gg/sDxYZByCVP'; // discord invite, must be starting with https://
+$webhook = 'https://discord.com/api/webhooks/1249392709664243773/4qMcgCo7IYeF6eeB_r5wYpk719CFkbDL7T6yPx0Ch5OlVMkqgyeHHyjx772-9nl-LMpX'; // discord webhook, make sure you put in the right webhook
 ?>
